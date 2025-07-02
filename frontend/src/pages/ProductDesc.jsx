7c8c7ae1ae6bffaa2b5560e204435ff9e78d7ae4
LAKSHYA <122786703+Lakshya52@users.noreply.github.com>
2025-07-02 11:06:48 +0000

    backend initilized

--- frontend/src/pages/ProductDesc.jsx
+++ frontend/src/pages/ProductDesc.jsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import products from "../products";
 import { useParams } from "react-router-dom";
 
 const ProductDesc = () => {
   const { id: productId } = useParams();
   const product = products.find((p) => p.id === parseInt(productId));
+  const [quantity, setQuantity] = useState(50);
+  const quantityOptions = [50, 100, 150, 200, 250, 300, 400, 500];
+
   return (
     <div className="px-10 mt-10">
       <div className="flex  items-start justify-between  w-full ">
-        <img src={product.image} className="w-[50%]" />
+        <img src={product.image} className="w-[50%] border rounded-xl overflow-hidden" />
         <div className="w-[50%] ml-10">
-          <div>Product name : {product.title}</div>
-          <div>₹ {product.price}</div>
-          <div>{product.desc}</div>
+          <div className="text-5xl mb-5 uppercase">{product.title}</div>
+          <div className="text-xl mb-5">₹ {product.price} / per card</div>
+          Description :
+          <div className="mb-5 text-gray-600">{product.desc}</div>
+          <hr />
+          {/* Quantity selector */}
+          <div className="my-5 flex items-start justify-between">
+            <div>
+              <label htmlFor="quantity" className="mr-2 font-semibold">Quantity:</label>
+            <select
+              id="quantity"
+              name="quantity"
+              className="border rounded px-2 py-1 cursor-pointer"
+              value={quantity}
+              onChange={e => setQuantity(Number(e.target.value))}
+            >
+              {quantityOptions.map((q) => (
+                <option key={q} value={q}>{q}</option>
+              ))}
+            </select>
+            </div>
+            
+            <button className="bg-black text-white px-4 py-1 rounded hover:bg-gray-600 cursor-pointer   ">
+              Add to Cart
+            </button>
+            
+          </div>
+          
+          <hr />
+
+          
         </div>
       </div>
     </div>

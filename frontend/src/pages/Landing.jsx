38e08d39478937ac7779d5f03134310fb211b6be
lakshya52 <lakshyachandaliya71@gmail.com>
2025-07-04 10:31:39 +0530

    data successfully coming through redux

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
 // import products from '../products'
 import Product from '../components/Product'
-import axios from 'axios'
+// import axios from 'axios' 
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllProducts } from '../actions/productAction'
 
 
 const Landing = () => {
-
-  const [products, setProducts] = useState([])
+  const getAllProductsState = useSelector(state => state.getAllProductsReducer)
+  const { products, error, loading } = getAllProductsState
+  // const [products, setProducts] = useState([])
   const dispatch = useDispatch()
   useEffect(() => {
     // axios.get('http://localhost:3000/api/products/getAllProducts').then(res => {
@@ -28,9 +29,22 @@ const Landing = () => {
 
 
     <div className='flex flex-wrap justify-center gap-4 p-4'>
-      {products.length && (products.map(product => {
+
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h1>Something went wrong</h1>
+      ) : (
+        products.map(product => (
+          <div key={product._id}><Product product={product} /></div>
+        ))
+      )}
+
+
+
+      {/* {products.length && (products.map(product => {
         return <div key={product._id}><Product product={product} /></div>
-      }))}
+      }))} */}
     </div>
   )
 }
